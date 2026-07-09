b193623fd1a261c5dfa7901110a3bf0cc0aa2351
Hyria Roman <hyrya.roman@gmail.com>
2026-07-09 22:25:04 +0300

    loading: fix

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,10 +3,11 @@
 import css from "./page.module.css";
 
 export default function Loading() {
-  return;
-  <main className={css.main}>
-    <div className={css.container}>
-      <p>Loading, please wait...</p>
-    </div>
-  </main>;
+  return (
+    <main className={css.main}>
+      <div className={css.container}>
+        <p>Loading, please wait...</p>
+      </div>
+    </main>
+  );
 }
