5064a1d4a8e11d4d10dbb1705f7bdc70ed5078cf
Hyria Roman <hyrya.roman@gmail.com>
2026-07-09 22:27:23 +0300

    tanstack: fix

--- components/TanStackProvider/TanStackProvider.tsx
+++ components/TanStackProvider/TanStackProvider.tsx
@@ -3,13 +3,13 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
-
 interface TanStackProviderProps {
   children: React.ReactNode;
 }
 
 export default function TanStackProvider({ children }: TanStackProviderProps) {
+  const queryClient = new QueryClient();
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
