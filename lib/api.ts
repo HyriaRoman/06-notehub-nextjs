c95619b07ca60610acbad1ac2b80e8ca1ff178b2
Hyria Roman <hyrya.roman@gmail.com>
2026-07-09 14:52:36 +0300

    api: add api access functions

---
+++ lib/api.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+import type { Note, NewNote, NoteId } from "../types/note.ts";
+
+const NOTEHUB_API = axios.create({
+  baseURL: "https://notehub-public.goit.study/api/",
+  headers: {
+    Authorization: `Bearer ${import.meta.env.NEXT_PUBLIC_NOTEHUB_TOKEN}`,
+  },
+  params: {
+    perPage: 12,
+  },
+});
+
+interface NotehubFetchResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+export async function fetchNotes(
+  query: string,
+  page: number,
+): Promise<NotehubFetchResponse> {
+  const res = await NOTEHUB_API.get<NotehubFetchResponse>("/notes", {
+    params: {
+      search: query,
+      page,
+    },
+  });
+  return res.data;
+}
+
+export async function createNote(newNote: NewNote): Promise<Note> {
+  const res = await NOTEHUB_API.post<Note>("/notes", newNote);
+  return res.data;
+}
+
+export async function deleteNote(noteId: NoteId): Promise<Note> {
+  const res = await NOTEHUB_API.delete<Note>(`/notes/${noteId}`);
+  return res.data;
+}
